6655b5fb027c29b06adfd708248c7f782a6b077c
edgarplazola <edgarplazola@gmail.com>
2019-07-13 20:50:41 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,8 @@ if (environment === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 }
 else {
-    urlDB = "mongodb+srv://edgarplazola:nji90okm@cluster0-v16z7.mongodb.net/cafe?retryWrites=true&w=majority";
+    // urlDB = "mongodb+srv://edgarplazola:nji90okm@cluster0-v16z7.mongodb.net/cafe?retryWrites=true&w=majority";
+    urlDB = process.env.MONG_URI
 }
 
 process.env.URLDB = urlDB;
